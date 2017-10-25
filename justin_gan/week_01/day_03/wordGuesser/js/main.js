e58ee70f1c4b06935917ada6ea155a7cc71f29a9
JustinGan <jgan91@gmail.com>
2017-10-25 16:26:06 +1100

    testing removal of letters array

--- justin_gan/week_01/day_03/wordGuesser/js/main.js
+++ justin_gan/week_01/day_03/wordGuesser/js/main.js
@@ -1,35 +1,42 @@
-// array of 10 funny english words
-const words = ['bumfuzzle', 'cattywampus', 'gardyloo', 'taradiddle', 'billingsgate', 'snickersnee', 'widdershins', 'collywobbles', 'gubbins', 'dipthong'];
-
 const getRandomInt = function ( min, max ) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; // max is exclusive and min is inclusive
 }
+
+// array of 10 funny english words
+const words = ['bumfuzzle', 'cattywampus', 'gardyloo', 'taradiddle', 'billingsgate', 'snickersnee', 'widdershins', 'collywobbles', 'gubbins', 'dipthong'];
+
+// use getRandomInt() to choose a random word from array
 const word = words[getRandomInt(0, words.length)];
+
+// make array of letters & array of underscores from random word chosen
 let letters = [];
-let guesses = [];
-let found;
+let correctGuesses = [];
 
 for (let i = 0; i < word.length; i++) {
   letters.push(word[i]);
-  guesses.push('_');
+  correctGuesses.push('_');
 }
 
+let allGuesses = [];
+let numGuesses = 0;
+
+
 const guessLetter = function ( letter ) {
-  found = false;
+  let found = false;
   // iterate through letters
   for (let i = 0; i < letters.length; i++) {
     // if guessed letter matches
     if ( letters[i] === letter ) {
       // update word to reflect match
-      guesses[i] = letter;
+      correctGuesses[i] = letter;
       found = true;
     }
   }
 
   // log guessed letters
-  console.log(guesses.join(""));
+  console.log(correctGuesses.join(""));
   // if new letter found
   if ( found ) {
     // congratulate user
@@ -37,7 +44,7 @@ const guessLetter = function ( letter ) {
   }
 
   // if no letters needed to be guessed
-  if ( guesses.join('') === letters.join('') ) {
+  if ( correctGuesses.join('') === letters.join('') ) {
     // congratulate user for winning
     console.log(`Congratulations! You've guessed the word ${ word }`);
   }
