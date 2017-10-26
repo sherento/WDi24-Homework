b589db0b703f9bf7e8f624825433d1127ffbea6e
chriswillphoto <christophernekich@gmail.com>
2017-10-26 21:50:41 +1100

    storytelling test added

--- christopher_nekich/week_01/day_04/Bank/js/main.js
+++ christopher_nekich/week_01/day_04/Bank/js/main.js
@@ -5,20 +5,20 @@ bank = {
     for (var i = 0; i < bank.accounts.length; i++) {
       total += bank.accounts[i].accountBalance;
     };
-    return total;
+    return (`This bank holds $${total} in total.`);
   },
   addAccount: function(name, balance){
     bank.accounts.push({
       accountName: name,
       accountBalance: balance
     });
-    console.log(`Account added for ${name}. Opening balance: $${balance}`)
+    return (`Account added for ${name}. Opening balance: $${balance}`);
   },
   deposit: function(name, deposit){
     for (var i = 0; i < bank.accounts.length; i++) {
       if(bank.accounts[i].accountName === name){        // finds appropriate account
         bank.accounts[i].accountBalance += deposit;     // adds deposit to existing balance
-        console.log(`$${deposit} deposited. Current balance for ${name} is $${bank.accounts[i].accountBalance}`)
+        return (`$${deposit} deposited. Current balance for ${name} is $${bank.accounts[i].accountBalance}`)
       }; // name finder if conditional
     }; // for loop
   },  // withdraw function
@@ -27,13 +27,18 @@ bank = {
       if(bank.accounts[i].accountName === name){
         if(withdrawal < bank.accounts[i].accountBalance){   // makes sure accounts cannot be negative
           bank.accounts[i].accountBalance -= withdrawal;    // withdraws amount from existing balance
-          console.log(`$${withdrawal} withdrawn. Current balance for ${name} is $${bank.accounts[i].accountBalance}`);
+          return (`$${withdrawal} withdrawn. Current balance for ${name} is $${bank.accounts[i].accountBalance}`);
         }else{
-          console.log('Insufficient funds available in account for that withdrawal amount');
+          return (`$${bank.accounts[i].accountBalance} available in account. Unable to withdraw $${withdrawal}`);
         }; //positive balance if else
       }; // name finder if conditional
     }; // for loop
   } // withdraw function
 }; // bank object
 
-console.log(bank.addAccount('Willy', 12))
+console.log(bank.addAccount('Willy', 12));
+console.log(bank.deposit('Willy', 500));
+console.log(bank.withdraw('Willy', 25));
+console.log(bank.withdraw('Willy', 1000));
+console.log(bank.addAccount('James', 20))
+console.log(bank.moneyCount());
