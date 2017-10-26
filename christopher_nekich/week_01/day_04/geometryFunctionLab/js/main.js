050e4d4fc4d22bad3950555d95044f79f406d5b7
chriswillphoto <christophernekich@gmail.com>
2017-10-26 15:14:28 +1100

    finished geometry part 2

--- christopher_nekich/week_01/day_04/geometryFunctionLab/js/main.js
+++ christopher_nekich/week_01/day_04/geometryFunctionLab/js/main.js
@@ -26,3 +26,52 @@ const area = function(shape){
 const perimeter = function(shape){
   return (2 * shape.width + 2 * shape.length);
 };
+
+
+// Part 2, Triangle
+//
+// Given the following a triangle object like the one below, write the following functions:
+//
+// isEquilateral - Returns whether the triangle is equilateral or not
+// isIsosceles - Returns whether the triangle is isosceles or not
+// area - Returns the area of the Triangle
+// isObtuse - Returns whether the triangle is obtuse or not
+
+const triangle = {
+  sideA: 3,
+  sideB: 5,
+  sideC: 4
+};
+
+const isEquilateral = function(shape){
+  if(shape.sideA === shape.sideB && shape.sideB === shape.sideC){       // if all three sides are equal
+    return true;
+  }else{
+    return false;
+  };
+};
+
+const isIsosceles = function(shape){
+  if(shape.sideA === shape.sideB || shape.sideB === shape.sideC){       // if at least two sides are equal
+    return true;
+  }else{
+    return false;
+  };
+};
+
+const areaTriangle = function(shape){
+  let s = (shape.sideA + shape.sideB + shape.sideC) / 2 ;      // calculates half of the triangles perimeter
+  let area = s * (s - shape.sideA) * (s - shape.sideB) * (s - shape.sideC);       // herons formula
+  return area;
+};
+
+const isObtuse = function(shape){
+  let sides = [shape.sideA, shape.sideB, shape.sideC];
+  sides = sides.sort();
+
+  if( (sides[0]**2) + (sides[1]**2) < sides[2]**2 ){        // triangle is obtuse if a**2 + b**2 < c**2 where c is the longest side
+    return true;
+  }else{
+    return false;
+  };
+};
