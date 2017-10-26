7c16cf6719b013e6c173e68e1f5e36f1663d51f8
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-10-26 16:07:27 +1100

    finished cash register

--- madeleine_milasas/week_01/day_04/js/main.js
+++ madeleine_milasas/week_01/day_04/js/main.js
@@ -30,6 +30,7 @@ const perimeter = function (rect) {
 
 
 // Testing
+console.log( `\nRECTANGLES` );
 console.log( `Is the rectangle a square: ${ isSquare(rectangle) }` );
 console.log( `Area of the rectangle: ${ area(rectangle) }` );
 console.log( `Perimeter: ${ perimeter(rectangle) }` );
@@ -75,11 +76,11 @@ const areaTriangle = function (t) {
 };
 const isObtuse = function (t) {  // if a2+b2<c2, then lengths a, b, and c make up the sides of an obtuse triangle (c is longest side)
   // separate longest side from other two by making array sorted
-  const sides = Object.values( t ); // get values of sides out of object into an array
+  const sides = Object.values(t); // get values of sides out of object into an array
   const sidesSorted = sides.sort();
   const longestSide = sidesSorted[ sidesSorted.length - 1 ];
   const sqrLongestSide = longestSide * longestSide;
-  const sqrOtherSides = (sidesSorted[0] * sidesSorted[0]) + (sidesSorted[1] * sidesSorted[1]);
+  const sqrOtherSides = ( sidesSorted[0] * sidesSorted[0] ) + ( sidesSorted[1] * sidesSorted[1] );
   if (sqrOtherSides < sqrLongestSide) {
     return true;
   } else {
@@ -91,16 +92,55 @@ const isObtuse = function (t) {  // if a2+b2<c2, then lengths a, b, and c make u
 
 
 // Testing
+console.log( `\nTRIANGLES` );
 console.log( `Is the triangle equilateral: ${ isEquilateral(triangle) }` );
 console.log( `Is the triangle isosceles: ${ isIsosceles(triangle) }` );
 console.log( `Area of triangle (rounded to nearest whole number): ${ areaTriangle(triangle) }` );
 console.log( `Is the triangle obtuse: ${ isObtuse(triangle) }` );
 
 
+// ************** CASH REGISTER ***********
+
+// Write a function called cashRegister that takes a shopping cart object.
+// The object contains item names and prices (itemName: itemPrice).
+// The function should return the total price of the shopping cart.
+
+// Function
+const cashRegister = function (cart) {
+  let total = 0;
+   // get all prices in their own array
+  const allPrices = Object.values(cart);
+  // add all prices together
+  for (let i = 0; i < allPrices.length; i++) {
+    total += allPrices[i];
+  }
+  return total.toFixed(2); // with 2 decimals places like a cash register
+};
 
 
+// Example object
+const cartForParty = {
+  banana: 1.25,
+  handkerchief: 0.99,
+  Tshirt: 25.01,
+  apple: 0.60,
+  nalgene: 10.34,
+  proteinShake: 22.36
+};
+// should total 60.55
 
+// New object
+const cartForSchool = {
+  textbook: 50,
+  pencil: 0.5,
+  bag: 20,
+  drinkBottle: 15
+};
 
+// Testing
+console.log( `\nCASH REGISTER` );
+console.log( `Your total will be $${cashRegister(cartForParty)}` );
+console.log( `Your total will be $${cashRegister(cartForSchool)}` );
 
 
 
