652dd304554f1a9587eb50acf4303f30a007f7ff
JustinGan <jgan91@gmail.com>
2017-10-26 14:44:12 +1100

    completed cashRegister

---
+++ justin_gan/week_01/day_04/cashRegister/js/main.js
@@ -0,0 +1,24 @@
+// The Cash Register
+//
+// Write a function called cashRegister that takes a shopping cart object. The object contains item names and prices (itemName: itemPrice). The function should return the total price of the shopping cart.
+
+let cartForParty = {
+  banana: "1.25",
+  handkerchief: ".99",
+  Tshirt: "25.01",
+  apple: "0.60",
+  nalgene: "10.34",
+  proteinShake: "22.36"
+};
+
+const cashRegister = function ( cart ) {
+  const items = Object.keys(cart);
+  let total = 0;
+  for (let i = 0; i < items.length; i++) {
+    let item = items[i];
+    total += parseFloat(cart[item]);
+  }
+  return total;
+}
+
+console.log(cashRegister(cartForParty));
