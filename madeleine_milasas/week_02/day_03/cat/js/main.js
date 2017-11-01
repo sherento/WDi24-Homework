34dea3f5802363665c283177794cd5bd4f15aeed
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-11-01 18:49:34 +1100

    adjusted cat walking forwards, made it stop, added second mirror cat

--- madeleine_milasas/week_02/day_03/cat/js/main.js
+++ madeleine_milasas/week_02/day_03/cat/js/main.js
@@ -1,23 +1,45 @@
 
+// Cat init position vars
+const posH = '40px';
+const posV = '300px';
+const size = '140px';
 
+
+// FIRST CAT INIT
 const cat = document.getElementById('cat');
 cat.style.position = 'absolute';
-cat.style.left = '40px';
-cat.style.top = '300px';
-cat.style.width = '90px';
+cat.style.left = posH;
+cat.style.top = posV;
+cat.style.width = size;
+
+// SECOND CAT INIT
+const catMirror = document.getElementById(`cat-mirror`);
+catMirror.style.position = 'absolute';
+catMirror.style.right = posH;
+catMirror.style.top = posV;
+catMirror.style.width = size;
+catMirror.style.transform = 'scaleX(-1)';
 
 
-const catWalkForwards = function () {
+
+const catWalk = function () {
   let oldLeft = parseFloat(cat.style.left);
   let oldTop = parseFloat(cat.style.top);
   let oldWidth = parseFloat(cat.style.width);
-  let newLeft = oldLeft + 0.4;
-  let newTop = oldTop + 0.1;
-  let newWidth = oldWidth + 0.16;
+  let newLeft = oldLeft + 0.7;
+  let newTop = oldTop + 0.2;
+  let newWidth = oldWidth * 1.001;
   cat.style.left = `${ newLeft }px`;
   cat.style.top = `${ newTop }px`;
   cat.style.width = `${ newWidth }px`;
+  if ( parseFloat(cat.style.left) > 340 ) {
+    window.clearInterval(timerId);
+    // go to still gif
+    cat.src = "images/cat-walk-frames/cat02.gif";
+  }
 };
 
 
-setInterval( catWalkForwards, 20 );
+
+// now call function anim
+const timerId = setInterval( catWalk, 20 );
