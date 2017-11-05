fdd798ce15276684e4601a7e68ef14280f383361
chriswillphoto <christophernekich@gmail.com>
2017-11-05 13:16:27 +1100

    starting to refactor

--- christopher_nekich/week_02/day_05/ga-bank/js/atm.js
+++ christopher_nekich/week_02/day_05/ga-bank/js/atm.js
@@ -2,102 +2,93 @@ let checking = 0
 let savings = 0
 let total = checking + savings
 
-const updateTotal = function(){
-  total = checking + savings
-}
-
 const wSavings = function(){ //withdraw savings
-  let $amount = parseFloat( $("#savings-amount").val() );
-  savings -= $amount;
-  savings = parseFloat(savings.toFixed(2))
-  $("#savings-balance").html(`$${savings}`);
+  let $amount = parseFloat( $("#savings-amount").val() ); // amount in input turned into float
+  savings -= $amount; // taking amount from total savings
+  savings = parseFloat(savings.toFixed(2))  // rounding savings to two decimal points and turning back into float
+  updater(); // changing text on screen to reflect savings total
 }
 
-const dSavings = function(){
+const dSavings = function(){  // deposit savings
+  if(savings === 0){  // if savings is at 0, change color of savings screen to reflect account having a positive balance.
+    $("#savings-balance").removeClass("zero");
+  };
   let $amount = parseFloat( $("#savings-amount").val() );
   savings += $amount;
   savings = parseFloat(savings.toFixed(2))
-  $("#savings-balance").text(`$${savings}`);
+  updater();
 }
 
 const wChecking = function(){
   let $amount = parseFloat( $("#checking-amount").val() );
   checking -= $amount;
   checking = parseFloat(checking.toFixed(2))
-  $("#checking-balance").text(`$${checking}`);
+  updater();
 }
 
 const dChecking = function(){
+  if(checking === 0){
+    $("#checking-balance").removeClass("zero")
+  };
   let $amount = parseFloat( $("#checking-amount").val() );
   checking += $amount;
   checking = parseFloat(checking.toFixed(2))
+  updater();
+}
+
+const updater = function(){
+  total = checking + savings
+  $("#savings-balance").text(`$${savings}`);
   $("#checking-balance").text(`$${checking}`);
+  if(checking === 0){
+    $("#checking-balance").addClass("zero")
+  }
+  if(savings === 0){
+    $("#savings-balance").addClass("zero")
+  }
 }
 
 
 
 $(document).ready(function(){
-  $("#savings-balance").html(`$${savings}`)
-  $("#checking-balance").html(`$${checking}`)
+  updater();
 
   $("#savings-withdraw").on("click", function(){
     if($("#savings-amount").val() > savings){ // if attempting to overdraft checking
-      if($("#savings-amount").val() < total){ // overdraft if amount is less than total
+      if($("#savings-amount").val() <= total){ // overdraft if amount is less than total
         $("#savings-balance").addClass("zero")
         checkingAmount = parseFloat($("#savings-amount").val() - savings)
         savings = 0
         checking -= checkingAmount
         checking = parseFloat(checking.toFixed(2))
-        total = checking + savings
-        $("#savings-balance").text(`$${savings}`);
-        $("#checking-balance").text(`$${checking}`);
+        updater();
       }
     }else{
       wSavings();
-      updateTotal();
-      if(savings == 0){
-        $("#savings-balance").addClass("zero")
-      }
     }
   })
 
   $("#savings-deposit").on("click", function(){
-    if(savings === 0){
-      $("#savings-balance").removeClass("zero")
-    }
     dSavings();
-    updateTotal();
   })
 
   $("#checking-withdraw").on("click", function(){
     if($("#checking-amount").val() > checking){ // if attempting to overdraft checking
-      if($("#checking-amount").val() < total){ // overdraft if amount is less than total
+      if($("#checking-amount").val() <= total){ // overdraft if amount is less than total
         $("#checking-balance").addClass("zero")
         savingsAmount = parseFloat($("#checking-amount").val() - checking)
         savings -= savingsAmount
         checking = 0
         savings = parseFloat(savings.toFixed(2))
         total = checking + savings
-        $("#checking-balance").text(`$${checking}`);
-        $("#savings-balance").text(`$${savings}`);
-
+        updater();
       }
     }else{
       wChecking();
-      updateTotal();
-      if(checking == 0){
-        $("#checking-balance").addClass("zero")
-      }
     }
   })
 
   $("#checking-deposit").on("click", function(){
-    if(checking === 0){
-      $("#checking-balance").removeClass("zero")
-    }
     dChecking();
-    updateTotal();
   })
-
-
 }) // <------- document ready
