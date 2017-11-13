61e08e636bb8f26010e48f1fb6e23e314de5bf42
ezra mizrahi <mishraki@gmail.com>
2017-11-13 16:30:56 +1100

    basic calculator completed

--- ezra_mizrahi/week_04/day_01/calc.rb
+++ ezra_mizrahi/week_04/day_01/calc.rb
@@ -17,26 +17,63 @@ def prompt(message)
   gets.chomp #implicit return
 end
 
-def sum_function(n1, n2)
-  n1 + n2
+def add_function(a, b)
+  a + b
 end
 
-def sub_function(n1, n2)
-  n1 - n2
+def sub_function(a, b)
+  a - b
 end
 
-def multiply_function(n1, n2)
-  n1 * n2
+def multiply_function(a, b)
+  a * b
 end
 
-def div_function(n1, n2)
-  n1 / n2
+def div_function(a, b)
+  a / b
 end
 
 def basic_calculator # should accept two arguments
   operations_menu
   operations_choice = prompt('please enter your selection: ').downcase
+
+  until operations_choice == 'q'
+    # show the appropriate operation
+    case operations_choice
+    when 'add'
+      puts "enter two numbers to add: "
+      num1 = gets.to_i
+      num2 = gets.to_i
+      puts add_function(num1, num2)
+    when 'subtract'
+      puts "enter two numbers to subtract: "
+      num1 = gets.to_i
+      num2 = gets.to_i
+      puts sub_function(num1, num2)
+    when 'multiply'
+      puts "enter two numbers to multiply: "
+      num1 = gets.to_i
+      num2 = gets.to_i
+      puts multiply_function(num1, num2)
+    when 'divide'
+      num1 = gets.to_i
+      num2 = gets.to_i
+      puts div_function(num1, num2)
+    else
+      puts 'INVALID SELECTION'
+    end
+
+    #show the operations menu again
+    operations_menu
+
+    #get the operations menu choices again
+    operations_choice = prompt('please enter your selection: ').downcase
+
+  end
+
 end
+
+puts "thank you for using this terrible calculator"
 
 ################################################################################
 
