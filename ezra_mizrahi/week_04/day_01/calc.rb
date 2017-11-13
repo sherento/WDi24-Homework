080837ba164a0d931ad0a11ad4d966480f065c07
ezra mizrahi <mishraki@gmail.com>
2017-11-13 16:58:56 +1100

    advanced functions added

--- ezra_mizrahi/week_04/day_01/calc.rb
+++ ezra_mizrahi/week_04/day_01/calc.rb
@@ -12,10 +12,21 @@ def operations_menu
   puts "(q) - quit!"
 end
 
+def advanced_menu
+  puts "square root"
+  puts "exponent"
+  puts "(q) - quit!"
+end
+
 def prompt(message)
   print message
   gets.chomp #implicit return
 end
+
+################################################################################
+##################################
+### basic calculator functions ###
+##################################
 
 def add_function(a, b)
   a + b
@@ -32,6 +43,8 @@ end
 def div_function(a, b)
   a / b
 end
+
+
 
 def basic_calculator # should accept two arguments
   operations_menu
@@ -75,6 +88,54 @@ end
 
 puts "thank you for using this terrible calculator"
 
+
+################################################################################
+#####################################
+### advanced calculator functions ###
+#####################################
+
+def square_function ( a )
+  Math.sqrt( a )
+end
+
+def exp_function ( a, b )
+  a ** b
+end
+
+
+def advanced_calculator
+  advanced_menu
+  advanced_choice = prompt('please enter your selection: ').downcase
+
+  until advanced_choice == 'q'
+    case advanced_choice
+    when "square root"
+      puts "enter your number: "
+      num1 = gets.to_i
+      puts square_function( num1 )
+    when "exponent"
+      puts "enter two numbers: "
+      num1 = gets.to_i
+      num2 = gets.to_i
+      puts exp_function(num1, num2)
+    else
+      puts "INVALID SELECTION"
+    end
+
+
+    #show the advanced menu again
+    advanced_menu
+
+    #get the operations menu choices again
+    advanced_choice = prompt('please enter your selection: ').downcase
+
+  end
+
+
+end
+
+puts "thank you for using this terrible calculator"
+
 ################################################################################
 
 main_menu
@@ -86,6 +147,8 @@ until menu_choice == 'q'
   case menu_choice
   when 'b'
     basic_calculator
+  when 'a'
+    advanced_calculator
   else
     puts "invalid selection"
   end
