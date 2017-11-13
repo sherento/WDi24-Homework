c74856ca484e0b7f056348b5ffed326dd52affd4
chriswillphoto <christophernekich@gmail.com>
2017-11-13 15:31:03 +1100

    start of ruby calculator

---
+++ christopher_nekich/week_04/day_01/calculator/calc.rb
@@ -0,0 +1,14 @@
+def main_menu
+  puts "(b) - basic calculator"
+  puts "(a) - advanced calculator"
+  puts "(q) - quit"
+end
+
+def prompt(message)
+  print message
+  gets.chomp # Implicit return
+end
+
+def basic_calculator
+  puts "Basic Calculator Coming Soon"
+end
