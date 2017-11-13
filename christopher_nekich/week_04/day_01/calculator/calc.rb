df93b0d121b08860d40f772933acf82fac93fcac
chriswillphoto <christophernekich@gmail.com>
2017-11-13 16:20:41 +1100

    finished adding exponents and sqrt

--- christopher_nekich/week_04/day_01/calculator/calc.rb
+++ christopher_nekich/week_04/day_01/calculator/calc.rb
@@ -10,8 +10,58 @@ def prompt(message)
 end
 
 def basic_calculator
-  puts "Basic Calculator Coming Soon"
+  operation = prompt "Pick an operator (+, -, *, /): "
+  case operation
+  when "+"
+    num1 = prompt("Enter a number: ").to_i
+    num2 = prompt("Enter a number to add to the first: ").to_i
+
+    puts "#{num1} + #{num2} = #{num1 + num2}"
+
+  when "-"
+    num1 = prompt("Enter a number: ").to_i
+    num2 = prompt("Enter a number to take away from the first: ").to_i
+
+    puts "#{num1} - #{num2} = #{num1 - num2}"
+
+  when "*"
+    num1 = prompt("Enter a number: ").to_i
+    num2 = prompt("Enter a number to multiply by the first: ").to_i
+
+    puts "#{num1} * #{num2} = #{num1 * num2}"
+
+  when "/"
+    num1 = prompt("Enter a number: ").to_i
+    num2 = prompt("Enter a number to divide the first by: ").to_i
+
+    puts "#{num1} / #{num2} = #{num1 / num2}"
+
+  else
+    puts "Invalid operator"
+  end
 end
+
+
+def advanced_calculator
+  puts "(sq) - Find the square root of a number"
+  puts "(ex) - Find the exponent of a number"
+  operation = prompt("Please enter your selection: ").downcase
+
+  case operation
+  when "sq"
+    num = prompt("Enter a number: ").to_i
+    puts "The square root of #{num} is #{Math.sqrt(num)}"
+
+  when "ex"
+    num = prompt("Enter a number: ").to_i
+    num2 = prompt("Enter an exponent: ").to_i
+
+    puts "#{num} to the power of #{num2} is #{num ** num2}"
+  else
+    puts "Invalid Selection"
+  end
+end
+
 
 
 main_menu
@@ -22,6 +72,8 @@ until menu_choice == "q"
   case menu_choice
   when "b"
     basic_calculator
+  when "a"
+    advanced_calculator
   else
     puts "Invalid Selection"
   end
