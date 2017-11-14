03438b9fd7fd5f90aef92c15c88baab98e294e9f
ezra mizrahi <mishraki@gmail.com>
2017-11-15 09:50:54 +1100

    have all possible journeys working, just need to fix main method

--- ezra_mizrahi/week_04/day_02/mta/mta.rb
+++ ezra_mizrahi/week_04/day_02/mta/mta.rb
@@ -22,14 +22,12 @@ line_6 = [ 'Grand Central', '33rd', '28th 6', '23rd 6', 'Union Square', 'Astor P
 
 
 
-# function to list stops on a single line
+# function to list and sum stops on a single line
 # it should get the index of the start station and index of the end station
 # and produce a string which contains the stops of the trip
 # ...
 # if the trip starts towards or at the end of a line (i.e. end of the array)
 # then reverse the lines and get the new index values and proceed as above
-
-# need to add case if there is a transfer
 
 def list_stations ( line_start, stn_start, line_end, stn_end )
 
@@ -48,13 +46,11 @@ def list_stations ( line_start, stn_start, line_end, stn_end )
     rev_array_of_stns = reversed_line[rev_start, rev_end].size # sums stops
     rev_string_of_stns = reversed_line[rev_start, rev_end].join(", ")
     puts "You must travel on the following stops: #{ rev_string_of_stns } to reach #{ stn_end }."
-    puts "This is a total of #{ rev_array_of_stns + 1 } stations."
+    puts "This is a total of #{ rev_array_of_stns } stations."
   end
 
 end
 
-list_stations line_n, 'Times Square', line_n, '23rd N'
-list_stations line_n, '8th N', line_n, '34th'
 
 # if line_start does not equal line_end
 #   do transfer function
@@ -71,10 +67,10 @@ def transfer ( line_start, stn_start, line_end, stn_end )
   first_half_transfer = line_start[first_stn, union_sq_1].join(", ")
   # sums stations
   first_half_sum = line_start[first_stn, union_sq_1].size
-  puts "You must travel on the following stations: #{ first_half_transfer } to reach Union Square."
+  puts "You must travel on the following stations: #{ first_half_transfer } and then transfer at Union Square."
   puts "This is a total of #{ first_half_sum } stations."
   #### first half of transfer
-  puts "You must then transfer at Union Square"
+  #
   #### second half of transfer
   # gets index of second half of transfer stations
   union_sq_2 = line_end.find_index("Union Square")
@@ -83,12 +79,42 @@ def transfer ( line_start, stn_start, line_end, stn_end )
   second_half_transfer = line_end[union_sq_2, last_stn].join(", ")
   # sums stations
   second_half_sum = line_end[union_sq_2, last_stn].size
-  puts "You must travel on the following stations: #{ second_half_transfer }."
+  puts "You must travel on the following stations: #{ second_half_transfer } to reach #{ stn_end }."
   puts "This is a total of #{ second_half_sum } stations."
 end
 
-transfer line_n, 'Times Square', line_l, '3rd'
+def reverse_transfer ( line_start, stn_start, line_end, stn_end )
+  #### first half of transfer
+  #### reverse line_start
+  rev_line_start = line_start.reverse
+  #### get index of first station
+  first_stn = line_start.reverse.find_index(stn_start)
+  union_sq_1 = line_start.reverse.find_index("Union Square")
+  #### list stations
+  first_half_transfer = rev_line_start[first_stn, union_sq_1].join(", ")
+  #### sums stations
+  first_half_sum = line_start[first_stn, union_sq_1].size
+  puts "You must travel on the following stations: #{ first_half_transfer } and then transfer at Union Square."
+  puts "This is a total of #{ first_half_sum } stations."
+  #### second half of transfer
+  #### reverse line_end
+  rev_line_end = line_end.reverse
+  #### get index of last station
+  last_stn = line_end.reverse.find_index(stn_end)
+  union_sq_2 = line_end.reverse.find_index("Union Square")
+  #### list stations
+  second_half_transfer = rev_line_end[union_sq_2, last_stn].join(", ")
+  #### sums stations
+  second_half_sum = line_end[union_sq_2, last_stn].size
+  puts "You must travel on the following stations: #{ second_half_transfer } to reach #{ stn_end }."
+  puts "This is a total of #{ second_half_sum } stations."
+end
 
+puts reverse_transfer line_l, '1st', line_n, '28th N'
+
+# main plan trip method
+# compares if start and end lines are the same or different, and decides
+# which method to use based on this
 def plan_trip line_start, stn_start, line_end, stn_end
   if line_start == line_end
     list_stations( line_start, stn_start, line_end, stn_end )
@@ -97,5 +123,14 @@ def plan_trip line_start, stn_start, line_end, stn_end
   end
 end
 
+# one line trip going forward
 plan_trip line_n, 'Times Square', line_n, '23rd N'
+
+# one line trip going backwards
+plan_trip line_n, '23rd N', line_n, 'Times Square'
+
+# multi line trip going forward
 plan_trip line_n, 'Times Square', line_l, '3rd'
+
+# multi line trip going backwards
+plan_trip line_l, '1st', line_n, 'Times Square'
