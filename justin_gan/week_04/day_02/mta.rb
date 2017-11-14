c36a7bb9e2feb21094356cbbf754810f167f3d19
JustinGan <jgan91@gmail.com>
2017-11-14 16:52:28 +1100

    refactored plan_trip with get_journey_leg

--- justin_gan/week_04/day_02/mta.rb
+++ justin_gan/week_04/day_02/mta.rb
@@ -1,15 +1,11 @@
 require 'pry'
 
-def plan_trip( line, start_station, end_station )
-  start_index = get_station_index( line, start_station )
-  end_index = get_station_index( line, end_station )
+def plan_trip( start_line, start_station, end_line, end_station )
+  start_index = get_station_index( SUBWAY[ start_line ], start_station )
+  end_index = get_station_index( SUBWAY[ end_line ], end_station )
 
-  if start_index == end_index
-    p "You're already at #{ end_station } station."
-  elsif start_index < end_index
-    travel_east( line, start_index, end_index )
-  else
-    travel_west( line, start_index, end_index )
+  if start_line == end_line
+    p get_journey_leg( SUBWAY[ start_line ], start_index, end_index, end_station )
   end
 end
 
@@ -17,21 +13,43 @@ def get_station_index( line, station )
   line.index( station )
 end
 
+def get_journey_leg( line, start_index, end_index, end_station )
+  if start_index == end_index
+    "You're already at #{ end_station } station."
+  elsif start_index < end_index
+    travel_east( line, start_index, end_index )
+  else
+    travel_west( line, start_index, end_index )
+  end
+end
+
 def travel_east( line, start_index, end_index )
-  p "travelling east"
   passed_stations = line[start_index..end_index]
-  p passed_stations
 end
 
 def travel_west( line, start_index, end_index )
-  p "travelling west"
-  passed_stations = line[end_index..start_index].reverse  # correct order
-  p passed_stations
-  binding.pry
+  # end index is smaller if travelling west
+  passed_stations = line[end_index..start_index].reverse
 end
 
-N = [ "Times Square", "34th", "28th", "23rd", "Union Square", "8th"]
+SUBWAY = {
+  :N => [ "Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
+  :L => [ "8th", "6th", "Union Square", "3rd", '1st' ],
+  6 => [ "Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place" ]
+}
 
-plan_trip N, '34th', 'Union Square'
-plan_trip N, '28th', '28th'
-plan_trip N, '8th', '23rd'
+
+plan_trip :N, '34th', :N, 'Union Square'
+plan_trip :N, '28th', :N, '28th'
+plan_trip :N, '8th', :N, '23rd'
+plan_trip :L, '8th', :L, '3rd'
+plan_trip :L, '6th', :L, '6th'
+plan_trip :L, '1st', :L, 'Union Square'
+plan_trip 6, '33rd', 6, 'Astor Place'
+plan_trip 6, '23rd', 6, '23rd'
+plan_trip 6, 'Union Square', 6, '33rd'
+
+
+
+
+binding.pry
