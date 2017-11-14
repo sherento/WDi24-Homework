0ebfc4283fc9b71c27f9bee55eaf55c0b74a8a73
JustinGan <jgan91@gmail.com>
2017-11-14 20:23:44 +1100

    both lines working

--- justin_gan/week_04/day_02/mta.rb
+++ justin_gan/week_04/day_02/mta.rb
@@ -7,8 +7,15 @@ def plan_trip( start_line, start_station, end_line, end_station )
   if start_line == end_line
     p get_journey_leg( SUBWAY[ start_line ], start_index, end_index, end_station )
   else
-    p "figure out 2 lines"
-    
+    # find intersecting station
+    intersect = (SUBWAY[ start_line ] & SUBWAY[ end_line ]).first
+    intersect_index_start_line = get_station_index( SUBWAY[ start_line ], intersect )
+    intersect_index_end_line = get_station_index( SUBWAY[ end_line ], intersect )
+    first_leg = get_journey_leg( SUBWAY[ start_line ], start_index, intersect_index_start_line, end_station )
+    second_leg = get_journey_leg( SUBWAY[ end_line ], intersect_index_end_line, end_index, end_station )
+    p "You must travel through the following stops on the #{ start_line } line: #{ first_leg.join(", ") }."
+    p "Change at #{ intersect }."
+    p "Your journey continues through the following stops on the #{ end_line }: #{ second_leg.join(", ") }."
   end
 end
 
@@ -31,7 +38,7 @@ def travel_east( line, start_index, end_index )
 end
 
 def travel_west( line, start_index, end_index )
-  # end index is smaller if travelling west
+  # end index is smaller if travelling west; reverse to get correct order
   passed_stations = line[end_index..start_index].reverse
 end
 
@@ -42,7 +49,7 @@ SUBWAY = {
 }
 
 
-plan_trip :N, '34th', 6, '28th'
+plan_trip :N, '34th', 6, '28th (6)'
 # plan_trip :N, '34th', :N, 'Union Square'
 # plan_trip :N, '28th', :N, '28th'
 # plan_trip :N, '8th', :N, '23rd'
@@ -52,8 +59,3 @@ plan_trip :N, '34th', 6, '28th'
 # plan_trip 6, '33rd', 6, 'Astor Place'
 # plan_trip 6, '23rd', 6, '23rd'
 # plan_trip 6, 'Union Square', 6, '33rd'
-
-
-
-
-binding.pry
