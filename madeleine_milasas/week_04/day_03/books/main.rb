f92d64779651dcc339747ad66af9ecbc9f362fab
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-11-15 17:40:11 +1100

    added results page w title author descrip and thumbnail

--- madeleine_milasas/week_04/day_03/books/main.rb
+++ madeleine_milasas/week_04/day_03/books/main.rb
@@ -6,3 +6,18 @@ require 'httparty'
 get '/' do
   erb :home
 end
+
+get '/book' do
+  redirect to('/') if params[:title].empty?
+
+  @title = params[:title]
+  url = "https://www.googleapis.com/books/v1/volumes?q=title:#{ @title }"
+  info = HTTParty.get(url)
+
+  @found_title = info['items'].first['volumeInfo']['title']
+  @authors = info['items'].first['volumeInfo']['authors']
+  @description = info['items'].first['volumeInfo']['description']
+  @image = info['items'].first['volumeInfo']['imageLinks']['smallThumbnail']
+
+  erb :book
+end
