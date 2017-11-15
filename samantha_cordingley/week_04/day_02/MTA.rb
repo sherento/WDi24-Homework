f6baf5cf325052f30f0e836c15498ebc5c3fae1f
Sam Cordingley <sam@cordandberg.com>
2017-11-15 16:46:40 +1100

    master function that calls correct function depending on number of lines and direction. Need to add user input and distinguish next

--- samantha_cordingley/week_04/day_02/MTA.rb
+++ samantha_cordingley/week_04/day_02/MTA.rb
@@ -35,7 +35,7 @@ def plan_trip(start_line, start_stop, end_line, end_stop)
   puts "Your trip is #{trip_length} stops in length. Start at the #{start_line}, and get on at platform #{start_stop}. Travel through stops #{user_journey} ,and get off at #{end_stop}."
 end
 
-plan_trip :n_line, '34th', :n_line, '8th' #forward on one line test
+# plan_trip :n_line, '34th', :n_line, '8th' #forward on one line test
 
 # ############REVERSE FUNCTION####################
 
@@ -54,7 +54,7 @@ def backwards_trip(start_line, start_stop, end_line, end_stop)
 end
 
 
-backwards_trip :n_line, 'union square', :n_line, '34th' #backwards on one line
+# backwards_trip :n_line, 'union square', :n_line, '34th' #backwards on one line
 
 
 ################MULTI LINE BOTH FORWARDS#########################
@@ -100,8 +100,8 @@ end
   puts "Get off at #{end_stop}."
 end
 
-plan_multi_trip :n_line, '34th', :l_line, '3rd' #changing at union sq and going forward
-plan_multi_trip :six_line, '33rd', :n_line, '8th' #changing at union sq and going forward
+# plan_multi_trip :n_line, '34th', :l_line, '3rd' #changing at union sq and going forward
+# plan_multi_trip :six_line, '33rd', :n_line, '8th' #changing at union sq and going forward
 
 ######################MULTI LINE START LINE FORWARDS, END LINE BACKWARDS############################
 
@@ -124,7 +124,7 @@ new_reverse_line = MTA[end_line].reverse!
 # print new_reverse_line
 start_line_position = new_reverse_line.index('union square')
 end_line_position = new_reverse_line.index(end_stop)
-second_leg = new_reverse_line[(start_line_position)...end_line_position]
+second_leg = new_reverse_line[(start_line_position +1)...end_line_position]
 end
 
 total_journey = first_leg + second_leg
@@ -143,8 +143,8 @@ end
   puts "Get off at #{end_stop}."
 end
 
-plan_multi_startforwards :n_line, '28th', :six_line, '28th' #starting in forwards motion, changing at union sq and going backwards
-plan_multi_startforwards :six_line, '23rd', :l_line, '8th' #starting in forwards motion, changing at union sq and going backwards
+# plan_multi_startforwards :n_line, '28th', :six_line, '28th' #starting in forwards motion, changing at union sq and going backwards
+# plan_multi_startforwards :six_line, '23rd', :l_line, '8th' #starting in forwards motion, changing at union sq and going backwards
 
 
 ######################MULTI LINE START LINE BACKWARDS, END LINE FORWARDS############################
@@ -192,20 +192,18 @@ end
 end
 
 # plan_multi_startbackwards :l_line, '1st', :n_line, '8th' #starting line going backwards, changing at union sq and going forwards
-plan_multi_startbackwards :n_line, '8th', :l_line, '3rd' #starting line going backwards, changing at union sq and going forwards
+# plan_multi_startbackwards :n_line, '8th', :l_line, '3rd' #starting line going backwards, changing at union sq and going forwards
 
 ######################MULTI LINE BOTH BACKWARDS############################
 
 def plan_multi_bothbackwards(start_line, start_stop, end_line, end_stop)
-  # start_line_position = MTA[start_line].index(start_stop)
-  # end_line_position = MTA[end_line].index(end_stop)
   if start_line != end_line
     first_leg = []
     first_reverse_line = []
     second_reverse_line = []
     second_leg = []
 
-    first_reverse_line = MTA[start_line]
+    first_reverse_line = MTA[start_line].reverse!
     # print first_reverse_line
     reverse_start_position = first_reverse_line.index(start_stop)
     reverse_end_position = first_reverse_line.index('union square')
@@ -213,7 +211,7 @@ def plan_multi_bothbackwards(start_line, start_stop, end_line, end_stop)
 
 # print first_leg
 
-  second_reverse_line = MTA[end_line]
+  second_reverse_line = MTA[end_line].reverse!
   # print second_reverse_line
   start_line_position = second_reverse_line.index('union square')
   end_line_position = second_reverse_line.index(end_stop)
@@ -237,12 +235,49 @@ end
   puts "Get off at #{end_stop}."
 end
 
-plan_multi_bothbackwards :six_line, 'astor place', :n_line, '28th' #starting line going backwards, changing at union sq and second line going backwards
+# plan_multi_bothbackwards :six_line, 'astor place', :n_line, '28th' #starting line going backwards, changing at union sq and second line going backwards
+#
+# plan_multi_bothbackwards :l_line, '1st', :six_line, '28th' #starting line going backwards, changing at union sq and second line going backwards
 
-plan_multi_bothbackwards :l_line, '1st', :six_line, '28th' #starting line going backwards, changing at union sq and second line going backwards
+#######################MASTER FUNCTION########################################
+
+def master_planner(start_line, start_stop, end_line, end_stop)
+  first_line_st_position = MTA[start_line].index(start_stop)
+  first_line_end_position = MTA[start_line].index('union square')
+  second_line_st_position = MTA[end_line].index('union square')
+  second_line_end_position = MTA[end_line].index(end_stop)
+  # puts first_line_st_position
+  # puts first_line_end_position
+  # puts second_line_st_position
+  # puts second_line_end_position
 
 
-######################INPUT FROM USER##################
+  if start_line == end_line && (first_line_st_position < first_line_end_position)
+    plan_trip start_line, start_stop, end_line, end_stop
+  elsif start_line == end_line && (first_line_st_position > first_line_end_position)
+    backwards_trip start_line, start_stop, end_line, end_stop
+  elsif start_line != end_line && (first_line_st_position < first_line_end_position) && (second_line_st_position < second_line_end_position)
+    plan_multi_trip start_line, start_stop, end_line, end_stop
+  elsif start_line != end_line && (first_line_st_position < first_line_end_position) && (second_line_st_position > second_line_end_position)
+    plan_multi_startforwards start_line, start_stop, end_line, end_stop
+  elsif start_line != end_line && (first_line_st_position > first_line_end_position) && (second_line_st_position < second_line_end_position)
+    plan_multi_startbackwards start_line, start_stop, end_line, end_stop
+  elsif start_line != end_line && (first_line_st_position > first_line_end_position) && (second_line_st_position > second_line_end_position)
+    plan_multi_bothbackwards start_line, start_stop, end_line, end_stop
+  else
+    puts "I don't understand, please try again"
+  end
+end
+
+master_planner(:n_line, '34th', :n_line, '8th') #forward on one line test
+master_planner(:n_line, 'union square', :n_line, '34th') #backwards on one line
+master_planner(:n_line, '34th', :l_line, '3rd') #changing at union sq and going forward
+master_planner(:n_line, '28th', :six_line, '28th') #starting in forwards motion, changing at union sq and going backwards
+master_planner(:l_line, '1st', :n_line, '8th') #starting line going backwards, changing at union sq and going forwards
+master_planner(:l_line, '1st', :six_line, '28th') #starting line going backwards, changing at union sq and second line going backwards
+master_planner(:six_line, 'astor place', :n_line, '34th') #starting line going backwards, changing at union sq and second line going backwards
+
+######################INPUT FROM USER########################################
 
 # def trip_info
 #   print "What is your start line? "
