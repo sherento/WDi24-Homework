20a7457c574eabf4bb044f395aec25d85da29467
JustinGan <jgan91@gmail.com>
2017-11-16 17:16:36 +1100

    updated style

--- justin_gan/week_04/day_03/books1/main.rb
+++ justin_gan/week_04/day_03/books1/main.rb
@@ -8,12 +8,18 @@ get ('/') do
 end
 
 get ('/book') do
-  @book = params[:book]
-  book_info = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=title:#{ @book }")["items"].first
-  volume_info = book_info["volumeInfo"]
-  @thumbnail = volume_info["imageLinks"]["thumbnail"]
-  @isbn = volume_info["industryIdentifiers"][0]["identifier"]
-  @buy_link = book_info["saleInfo"]["buyLink"]
+  begin
+    @book = params[:book]
+    book_info = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=title:#{ @book }")["items"].first
+    volume_info = book_info["volumeInfo"]
+    @title = volume_info["title"]
+    @authors = volume_info["authors"]
+    @thumbnail = volume_info["imageLinks"]["thumbnail"]
+    @isbn = volume_info["industryIdentifiers"][0]["identifier"]
+    @buy_link = book_info["saleInfo"]["buyLink"]
+  rescue
+    redirect to('/')
+  end
 
   # binding.pry
   erb :book
