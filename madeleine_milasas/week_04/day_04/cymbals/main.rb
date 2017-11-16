6f5714b49b0bd6ad1c6d6fa107e77df26f21cf06
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-11-16 18:49:44 +1100

    made index and single item show pages

--- madeleine_milasas/week_04/day_04/cymbals/main.rb
+++ madeleine_milasas/week_04/day_04/cymbals/main.rb
@@ -4,5 +4,31 @@ require 'sqlite3'
 require 'pry'
 
 get '/' do
-  "Working"
+  erb :home
+end
+
+# index
+get '/cymbals' do
+  @cymbals = query_db 'SELECT * FROM cymbals'
+  erb :cymbals_index
+end
+
+# show
+get '/cymbals/:id' do
+  @cymbal = query_db("SELECT * FROM cymbals WHERE id = #{ params[:id] }").first
+  erb :cymbals_show
+end
+
+
+
+
+
+
+def query_db sql_statement
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.results_as_hash = true
+  puts sql_statement
+  results = db.execute sql_statement
+  db.close
+  results
 end
