60b9ebc32ca59481a5f5aecab6e2455843f90aa6
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-11-16 19:33:18 +1100

    added new input form

--- madeleine_milasas/week_04/day_04/cymbals/main.rb
+++ madeleine_milasas/week_04/day_04/cymbals/main.rb
@@ -13,6 +13,16 @@ get '/cymbals' do
   erb :cymbals_index
 end
 
+# new
+get '/cymbals/new' do
+  erb :cymbals_new
+end
+
+post '/cymbals' do
+  query_db("INSERT INTO cymbals (maker, name, type, sound, size) VALUES ('#{ params['maker'] }', '#{ params['name'] }', '#{ params['type'] }', '#{ params['sound'] }', '#{ params['size'] }')")
+  redirect to('/cymbals') # back to index page
+end
+
 # show
 get '/cymbals/:id' do
   @cymbal = query_db("SELECT * FROM cymbals WHERE id = #{ params[:id] }").first
