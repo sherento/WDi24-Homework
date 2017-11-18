0178a1d7f07a6a62300598a6a347c6c003c63c39
Damien Lowe <ashleydamienlowe@gmail.com>
2017-11-18 15:40:31 +1100

    Book search sinatra

--- damien_lowe/week_04/day_03/books1/main.rb
+++ damien_lowe/week_04/day_03/books1/main.rb
@@ -1,12 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
 require "pry"
+require "httparty"
 
 get "/" do
-  erb :home
+  erb :forms
 end
 
-get "/forms" do
-  
-  erb :forms
+get "/display" do
+
+  title = params[:title]
+  url = "https://www.googleapis.com/books/v1/volumes?q=title:#{title}"
+  info = HTTParty.get url
+  @cover_image = info['items'].first['volumeInfo']['imageLinks']['thumbnail']
+  @saleInfo = info["items"][1]["saleInfo"]
+  @description = info["items"].first["volumeInfo"]["description"]
+  @retail_price = info["items"][0]["saleInfo"]["retailPrice"]["amount"]
+  @author = info["items"][0]["volumeInfo"]["authors"][0]
+  erb :display
 end
