34c7ed6bea9a8b5a24a5e948777fd3029e99414a
Damien Lowe <ashleydamienlowe@gmail.com>
2017-11-19 11:12:38 +1100

    the budding

---
+++ damien_lowe/week_04/day_04/buds/table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE buds  (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  family TEXT,
+  image TEXT --URL for photo
+);
+
+--seed data
+INSERT INTO butterflies (name, family) VALUES ("Shiva Skunk", "Indica Hybrid");
