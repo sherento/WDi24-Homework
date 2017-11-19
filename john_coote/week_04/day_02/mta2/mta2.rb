d3eed77745b788a984f6fea392135a97d66cd79c
John Coote <john_coote@rocketmail.com>
2017-11-19 23:54:02 +1100

    finished mta2

--- john_coote/week_04/day_02/mta2/mta2.rb
+++ john_coote/week_04/day_02/mta2/mta2.rb
@@ -54,7 +54,8 @@ stations = {
 
 
 # start with a staion, later we'll feed this in via argument.
-queue = ['Cabramatta']   # array of stations to examine
+start = 'Blacktown'
+queue = [start]   # array of stations to examine
 destination = 'Brooklyn'
 
 list = []                # array of stations visited
@@ -64,6 +65,7 @@ childs_plus_parents = []
 
 while queue.length > 0
   station_to_examine = queue[0]                         # is this the destination?
+
   children = stations[station_to_examine][:connections] # if not, get it's children
   children = children - list                            # if children in list remove those
 
@@ -87,28 +89,63 @@ while queue.length > 0
   # ...........................................
 
 
-  puts station_to_examine
-  puts "queue #{queue}"
-  puts "list #{list}"
-  puts "stupid #{stupid}"
+  # puts "station to examine " + station_to_examine
+  # puts "queue #{queue}"
+  # puts "list #{list}"
+  # puts "stupid #{stupid}"
 
-binding.pry
+  if station_to_examine == destination
+    break                                               # if yes, break out of this loop
+  end
+
+# binding.pry
 end
 
 # now we have to pick apart the unwieldyl array trip to find the path.
 # If there's not a better way to do this, then I'm a monkey's uncle
 
-# it's the last element that contains the destination and it';s immediate parent
-child_parent_pair = stupid.pop # or stupid.last
-cp_length = child_parent_pair.length
-cp_split_index = child_parent_pair.index(",")
-child = child_parent_pair.slice(cp_split_index + 1, cp_length)
-parent = child_parent_pair.slice(0, cp_split_index)
+trip.unshift(destination)
 
-# shoves the child into the first position of trip, and then the parent following that
-trip.unshift(child)
-trip.unshift(parent)
+puts "stupid: " + stupid.to_s
+puts "trip: " + trip.to_s
+# binding.pry
 
+
+while stupid.length > 0
+
+# it's the last element that contains the destination and it';s immediate parent xxxx
+
+i = 0
+  while i < stupid.length
+    child_parent_pair = stupid[i]
+    cp_length = child_parent_pair.length
+    cp_split_index = child_parent_pair.index(",")
+    child = child_parent_pair.slice(cp_split_index + 1, cp_length)
+    parent = child_parent_pair.slice(0, cp_split_index)
+
+
+    if child == trip[0]
+      trip.unshift(parent)
+      stupid.pop(stupid.length - i - 1)
+      break
+    end
+
+    i = i + 1
+
+  # puts i
+  # puts "child: " + child.to_s
+  # puts "stupid: " + stupid.to_s
+  # puts "trip: " + trip.to_s
+  # binding.pry
+  end
+
+if ((trip[0] == start) and (trip[trip.length - 1] == destination))
+  break
+end
+
+end
+
+puts trip
 
 ## then
 # going backwards in the array stupid - find the next element that contains parent
@@ -116,4 +153,4 @@ trip.unshift(parent)
 # get it's index in the array stupid  - INDEX
 # repeat the above
 
-binding.pry
+#binding.pry
