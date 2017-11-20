ec63222e5030f44e5582e403bdad88f46b4b27f0
Sam Cordingley <sam@cordandberg.com>
2017-11-20 21:59:26 +1100

    secret number game shows your guess and secret number but doesnt show winner message

--- samantha_cordingley/week_05/day_01/game_park/app/controllers/secret_controller.rb
+++ samantha_cordingley/week_05/day_01/game_park/app/controllers/secret_controller.rb
@@ -5,10 +5,10 @@ class SecretController < ApplicationController
   def answer
     @guess = params[:guess]
     @secret_number = 1 + rand(10)
-    if @guess == @secret_number
-      @result = "You're a genius, you got it!"
-    else
-      @result = "Wrong!"
-    end
+  #   if @guess == @secret_number
+  #     @result = "Correct!"
+  #   else
+  #     @result = "Wrong!"
+  # end
   end
 end
