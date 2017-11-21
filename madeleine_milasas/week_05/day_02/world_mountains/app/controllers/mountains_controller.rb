0d9b0e8a21efa5da8bdad021aa65dbeee727b836
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-11-21 16:10:07 +1100

    added show, new and create

--- madeleine_milasas/week_05/day_02/world_mountains/app/controllers/mountains_controller.rb
+++ madeleine_milasas/week_05/day_02/world_mountains/app/controllers/mountains_controller.rb
@@ -1,4 +1,25 @@
 class MountainsController < ApplicationController
   def index
+    @mountains = Mountain.all
+  end
+
+  def new
+  end
+
+  def create
+    mountain = Mountain.new
+    mountain.name = params[:name]
+    mountain.image = params[:image]
+    mountain.base_height = params[:base_height]
+    mountain.sea_height = params[:sea_height]
+    mountain.location = params[:location]
+    mountain.fact = params[:fact]
+    mountain.save
+
+    redirect_to mountains_path
+  end
+
+  def show
+    @mountain = Mountain.find params[:id]
   end
 end
--- madeleine_milasas/week_05/day_02/world_mountains/config/routes.rb
+++ madeleine_milasas/week_05/day_02/world_mountains/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get '/mountains' => 'mountains#index'
+  root :to => 'mountains#index'
+  get '/mountains' => 'mountains#index' # index of all mountains
+  get '/mountains/new' => 'mountains#new'  # show form for new mountain
+  post '/mountains' => 'mountains#create' # makes a new mountain arise from the earth
+  get '/mountains/:id' => 'mountains#show', :as => "mountain" # show single mountain info
 end
