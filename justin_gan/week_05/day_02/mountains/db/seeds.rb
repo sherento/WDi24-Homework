2db615d128f517a0bae3b62c9e479397f5c4ad3d
JustinGan <jgan91@gmail.com>
2017-11-22 06:04:28 +1100

    basic layout complete

--- justin_gan/week_05/day_02/mountains/db/seeds.rb
+++ justin_gan/week_05/day_02/mountains/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Crag.destroy_all
+
+Crag.create :name => 'El Capitan', :location => 'Mariposa County, California, U.S.', :elevation => 2308, :parent_range => 'Sierra Nevada', :mountain_type => 'granite rock'
+Crag.create :name => 'Half Dome', :location => 'Mariposa County, California, U.S.', :elevation => 2695, :parent_range => 'Sierra Nevada', :mountain_type => "granite dome"
+Crag.create :name => 'Half Dome', :location => 'Mariposa County, California, U.S.', :elevation => 2308, :parent_range => 'Sierra Nevada', :mountain_type => "granite dome"
