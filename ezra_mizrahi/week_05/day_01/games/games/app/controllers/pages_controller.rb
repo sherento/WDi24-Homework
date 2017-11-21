db2f2fbdfe712917c5343e792003cf0b9f9fc8f6
ezra mizrahi <mishraki@gmail.com>
2017-11-22 08:55:35 +1100

    day02 crud system homework

---
+++ ezra_mizrahi/week_05/day_01/games/games/app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def eight
+  end
+
+  def answer
+  end
+
+end
---
+++ ezra_mizrahi/week_05/day_02/film_project/db/films.sql
@@ -0,0 +1,10 @@
+CREATE TABLE films (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  year INTEGER,
+  director TEXT,
+  synopsis TEXT,
+  language TEXT,
+  genre TEXT,
+  image TEXT
+);
