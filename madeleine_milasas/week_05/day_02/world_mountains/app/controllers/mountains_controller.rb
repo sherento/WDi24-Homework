92e00d29428c232e168d27aa18f60de74f83449c
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-11-21 21:37:32 +1100

    styled, also added extra nav buttons within pages

--- madeleine_milasas/week_05/day_02/world_mountains/app/controllers/mountains_controller.rb
+++ madeleine_milasas/week_05/day_02/world_mountains/app/controllers/mountains_controller.rb
@@ -1,6 +1,6 @@
 class MountainsController < ApplicationController
   def index
-    @mountains = Mountain.all
+    @mountains = Mountain.all.sort_by { |m| [m.name.downcase] }
   end
 
   def new
@@ -30,7 +30,7 @@ class MountainsController < ApplicationController
   def update
     mountain = Mountain.find params[:id]
     mountain.update :name => params[:name], :image => params[:image], :base_height => params[:base_height], :sea_height => params[:sea_height], :location => params[:location], :fact => params[:fact]
-  
+
     redirect_to mountain_path(mountain.id)
   end
 
