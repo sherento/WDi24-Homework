a869b7328197021406259a1c9c90041e51668d9a
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-04 15:44:22 +1100

    added title search

--- madeleine_milasas/week_07/day_01/books_ajax/js/main.js
+++ madeleine_milasas/week_07/day_01/books_ajax/js/main.js
@@ -1,24 +1,33 @@
 
 
 const fetchBook = function () {
+  // get search input
+  const search = document.getElementById('title').value;
   // make new xhr obj
   const xhr = new XMLHttpRequest();
-
+  // xhr state change *async
   xhr.onreadystatechange = function () {
     if ( xhr.readyState !== 4 ) {
       return;
     }
     // convert string into object
-     const book = JSON.parse( xhr.response );
-     const bookCover = book.items[0].volumeInfo.imageLinks.thumbnail;
+    const book = JSON.parse( xhr.response );
+    const bookCover = book.items[0].volumeInfo.imageLinks.thumbnail;
+    // remove previous image
+    prevImg = document.querySelector( 'img' );
+    if (prevImg) {
+      prevImg.parentNode.removeChild( prevImg );
+    }
     // create img element and append
     const img = document.createElement( 'img' );
     img.src = bookCover;
     document.body.appendChild( img );
   }
-  xhr.open( 'GET', 'https://www.googleapis.com/books/v1/volumes?q=Glamorama' );
+  // set up xhr and make request *async
+  xhr.open( 'GET', `https://www.googleapis.com/books/v1/volumes?q=${ search }` );
   xhr.send();
-
 };
 
-fetchBook();
+
+const button = document.getElementById('search');
+button.addEventListener('click', fetchBook );
