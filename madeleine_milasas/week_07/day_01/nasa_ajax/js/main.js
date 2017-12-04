2ea579e079760369cbfb8eea291ac0bb47b63398
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-04 20:07:15 +1100

    refactored, added alert for no images

--- madeleine_milasas/week_07/day_01/nasa_ajax/js/main.js
+++ madeleine_milasas/week_07/day_01/nasa_ajax/js/main.js
@@ -1,30 +1,72 @@
-const button = document.getElementById( 'search' );
 
-const fetchSpace = function () {
-  const searchYear = document.getElementById( 'search-year' ).value;
-  // new xhr obj
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if ( xhr.readyState !== 4 ) {
-      return;
-    }
-    // convert string to obj
-    const spaceInfo = JSON.parse( xhr.response );
-    console.log('spaceInfo', spaceInfo);
-    // get img link
-    const imgSrc = spaceInfo.collection.items[0].links[0].href;
-    // remove prev node
+// Set input field max to current year
+const currentYear = ( new Date() ).getFullYear();
+document.getElementById( 'search-year' ).setAttribute( 'max', currentYear );
+
+//*********************  DOM Actions ******************************//
+
+const DOM = {
+  button: document.getElementById( 'search' ),
+
+  removePrevious: function () {
     const prevImg = document.querySelector('img');
     if ( prevImg ) {
-      prevImg.parentNode.removeChild( prevImg );
+      prevImg.remove();
     }
-    // create and append node
+    const prevDescrip = document.querySelector( 'p' );
+    if (prevDescrip) {
+      prevDescrip.remove();
+    }
+  },
+
+  noneAlert: function () {
+    const alert = document.createElement( 'p' );
+    alert.innerText = "Sorry, no results for that year."
+    document.body.appendChild( alert );
+    this.button.innerText = "Again, again."
+  },
+
+  addImg: function (src) {
     const img = document.createElement( 'img' );
-    img.src = imgSrc;
-    console.log( img );
+    img.src = src;
     document.body.appendChild( img );
-    // change button text
-    button.innerText = "Again, again."
+  },
+
+  addDescrip: function (src) {
+    const descrip = document.createElement( 'p' );
+    descrip.innerText = src;
+    document.body.appendChild( descrip );
+  }
+};
+
+
+////       ********************  MAIN FETCH  **********************         ////
+
+
+const fetchSpace = function () {
+  DOM.button.innerText = "It's coming..."
+  const searchYear = document.getElementById( 'search-year' ).value;
+  // start xhr...
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if ( xhr.readyState !== 4 ) return;
+    DOM.removePrevious();
+    const spaceInfo = JSON.parse( xhr.response );
+    // choose random image from that year
+    const numImages = spaceInfo.collection.items.length;
+    if ( 0 === numImages ) {
+      DOM.noneAlert();
+      return;
+    }
+    const seed = Math.floor( Math.random() * numImages );
+    const chosen = spaceInfo.collection.items[ seed ];
+    // add image
+    const imgSrc = chosen.links[0].href;
+    DOM.addImg( imgSrc );
+    // add description
+    const descripSrc = chosen.data[0].description;
+    DOM.addDescrip( descripSrc );
+    DOM.button.innerText = "Again, again."
   };
   xhr.open( 'GET', `https://images-api.nasa.gov/search?media_type=image&year_start=${ searchYear }&year_end=${ searchYear }` );
   xhr.send();
@@ -32,4 +74,8 @@ const fetchSpace = function () {
 
 
 
-button.addEventListener( 'click', fetchSpace );
+//////////////////////////////////////
+////        Events                ////
+//////////////////////////////////////
+
+DOM.button.addEventListener( 'click', fetchSpace );
