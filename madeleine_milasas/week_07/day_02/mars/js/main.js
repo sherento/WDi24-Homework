af5595780c143e0a6536c9127e70a295419876ec
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-05 19:00:17 +1100

    added getting mars image

--- madeleine_milasas/week_07/day_02/mars/js/main.js
+++ madeleine_milasas/week_07/day_02/mars/js/main.js
@@ -1,1 +1,30 @@
-console.log('works');
+const fetchMars = function (e) {
+  e.preventDefault();
+
+  $('#search').text('Searching...');
+
+  // get the query value
+  const date = $('#query').val();   // yyyy-mm-dd
+
+  // get json from api
+  $.ajax(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${ date }&api_key=9iGkM4XpqNHAJL5c7cSp4SBHDaLlNpDZJClwHlfz`)
+    .done( function (rover) {
+      if ( rover.photos.length > 0 ) {
+        $('#alert').text( '' );  // remove any prev alert
+        const marsImage = rover.photos[0].img_src;
+        $('#mars-image').attr('src', marsImage);
+        $('#earth-image').attr('src', marsImage);
+        $('#search').text('Show me a new date');
+      }
+    }).fail( function () {
+      $('#alert').text( 'Sorry, no images for that date.' );
+      $('#search').text('Show me a different date');
+    });
+}; // end fetchMars
+
+
+
+///************    EVENT HANDLER ****************//////
+$(document).ready( function () {
+  $('#date-search').on('submit', fetchMars );
+});
