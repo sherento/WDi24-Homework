c2250fb3d5ec81a117c92ac5a316bf11d9a3461d
JustinGan <jgan91@gmail.com>
2017-12-05 22:07:39 +1100

    added link to board game

--- justin_gan/week_07/day_02/js/boardGames.js
+++ justin_gan/week_07/day_02/js/boardGames.js
@@ -2,17 +2,18 @@
 const fetchBoardGames = function () {
   $.getJSON( 'https://bgg-json.azurewebsites.net/hot' ).done( function ( info ) {
     info.forEach( function ( game ) {
+      const $link = $( '<a>');
+      $link.attr( 'href', `https://boardgamegeek.com/boardgame/${ game.gameId}/${ game.name }` )
+      $link.attr( 'target', '_blank')
+
       const $image = $( '<img>' );
       $image.attr( 'src', game.thumbnail );
       $image.css( 'margin-bottom', '10px' );
-      $( '#board-games' ).append( $image );
+
+      $link.append( $image )
+      $( '#board-games' ).append( $link );
     });
   } );
 }
-
-
-
-
-
 
 $( document ).ready( fetchBoardGames );
