8454ac5a956cf52ec295b0958efbbce5b45a9c5c
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-05 20:13:05 +1100

    added second rover, fixed clearing of prev images and msgs

--- madeleine_milasas/week_07/day_02/mars/js/main.js
+++ madeleine_milasas/week_07/day_02/mars/js/main.js
@@ -1,25 +1,54 @@
 const fetchMars = function (e) {
   e.preventDefault();
 
-  $('#search').text('Searching...');
-
   // get the query value
   const date = $('#query').val();   // yyyy-mm-dd
+
+  const noCuriosity = function () {
+    $('#curiosity-alert').html( '* No images from <em>Curiosity</em> on this date.' );
+    $('#curiosity-image').attr('src', ''); // remove prev image
+    $('#curiosity-image').attr('alt', '');
+  };
+
+  const noOpportunity = function () {
+    $('#opportunity-alert').html( '* No images from <em>Opportunity</em> on this date.' );
+    $('#opportunity-image').attr('src', ''); // remove prev image
+    $('#opportunity-image').attr('alt', '');
+  };
 
   // get json from api
   $.ajax(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${ date }&api_key=9iGkM4XpqNHAJL5c7cSp4SBHDaLlNpDZJClwHlfz`)
     .done( function (rover) {
       if ( rover.photos.length > 0 ) {
-        $('#alert').text( '' );  // remove any prev alert
+        $('#curiosity-alert').text( 'Curiosity' );  // show name, remove any prev alert
         const marsImage = rover.photos[0].img_src;
-        $('#mars-image').attr('src', marsImage);
-        $('#earth-image').attr('src', marsImage);
-        $('#search').text('Show me a new date');
+        $('#curiosity-image').attr('src', marsImage);
+        $('#curiosity-image').attr('alt', 'Image from the Curiosity Rover');
+      } else {
+        noCuriosity();
       }
     }).fail( function () {
-      $('#alert').text( 'Sorry, no images for that date.' );
-      $('#search').text('Show me a different date');
+        noCuriosity();
     });
+
+    $.ajax(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?earth_date=${ date }&api_key=9iGkM4XpqNHAJL5c7cSp4SBHDaLlNpDZJClwHlfz`)
+      .done( function (rover) {
+        if ( rover.photos.length > 0 ) {
+          $('#opportunity-alert').text( 'Opportunity' );  // show name, remove any prev alert
+          const marsImage = rover.photos[0].img_src;
+          $('#opportunity-image').attr('src', marsImage);
+          $('#opportunity-image').attr('alt', 'Image from the Opportunity Rover');
+        } else {
+          noOpportunity();
+        }
+      }).fail( function () {
+        noOpportunity();
+      });
+
+
+
+
+
 }; // end fetchMars
 
 
